e23aa25aa18c616c04a0892d203ada3931470420
Jenna Caldwell <jenna.caldwell@astrazeneca.com>
2022-02-16 10:12:54 -0500

    Cleaned up syntax

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,8 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-...
+
+
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -14,6 +15,7 @@ def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -40,12 +42,14 @@ def test_daily_min(test, expected):
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
+
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 @pytest.mark.parametrize(
     "test, expected, expect_raises",
